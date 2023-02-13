1273edfaf3ffdbe1d8d32924cb259db60d00eb3b
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-02-13 15:42:46 +0200

    add organization section

--- src/slider.js
+++ src/slider.js
@@ -1,1 +1,12 @@
 //Slider
+var swiper = new Swiper('.mySwiper', {
+  loop: true,
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  // },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
