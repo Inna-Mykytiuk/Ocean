ae03eb8f4c8f348fd57eecf74dbf12c148824aef
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-02-13 19:17:40 +0200

    add video bg

--- src/slider.js
+++ src/slider.js
@@ -10,3 +10,21 @@ var swiper = new Swiper('.mySwiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+// breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       slidesPerView: 2,
+//       spaceBetween: 20, //width between sliders
+//     },
+//     // when window width is >= 480px
+//     480: {
+//       slidesPerView: 3,
+//       spaceBetween: 30,
+//     },
+//     // when window width is >= 640px
+//     640: {
+//       slidesPerView: 4,
+//       spaceBetween: 40,
+//     },
+//   },
