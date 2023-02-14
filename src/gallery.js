5ca550199d9e6b73005b7c10b47d7da9105a0611
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-02-14 12:26:04 +0200

    update gallery style

---
+++ src/gallery.js
@@ -0,0 +1,27 @@
+// const images = document.querySelectorAll('.lightbox-gallery img');
+// const lightbox = document.querySelector('.lightboxs');
+// const mainImg = document.querySelector('.lightboxs img');
+// const arrLeft = document.querySelector('.arrow-left');
+// const arrRight = document.querySelector('.arrow-right');
+
+// let imgIndex = 0;
+
+// images.forEach(img => {
+//   img.addEventListener('click', e => {
+//     mainImg.src = e.target.src;
+//     imgIndex = [...images].indexOf(img);
+//     lightbox.style.display = 'flex';
+//     setTimeout(() => {
+//       lightbox.style.opacity = '1';
+//     }, 10);
+//   });
+// });
+
+// window.addEventListener('click', e => {
+//   if (e.target.classList.contains('lightboxs')) {
+//     lightbox.style.opacity = '0';
+//     setTimeout(() => {
+//       lightbox.style.opacity = 'none';
+//     }, 350);
+//   }
+// });
