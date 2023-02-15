274348de68f17e76d1bca68bbf91c47d45f2259e
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-02-15 22:47:40 +0200

    update file

--- src/nav.js
+++ src/nav.js
@@ -20,24 +20,24 @@ $(function () {
     }
   }
 
-  /* Smooth scroll */
-  $('[data-scroll]').on('click', function (event) {
-    event.preventDefault();
+  // /* Smooth scroll */
+  // $('[data-scroll]').on('click', function (event) {
+  //   event.preventDefault();
 
-    var $this = $(this),
-      blockId = $this.data('scroll'),
-      blockOffset = $(blockId).offset().top;
+  //   var $this = $(this),
+  //     blockId = $this.data('scroll'),
+  //     blockOffset = $(blockId).offset().top;
 
-    $('#nav a').removeClass('active');
-    $this.addClass('active');
+  //   $('#nav a').removeClass('active');
+  //   $this.addClass('active');
 
-    $('html, body').animate(
-      {
-        scrollTop: blockOffset,
-      },
-      500
-    );
-  });
+  //   $('html, body').animate(
+  //     {
+  //       scrollTop: blockOffset,
+  //     },
+  //     500
+  //   );
+  // });
 
   /* Menu nav toggle */
   $('#nav_toggle').on('click', function (event) {
@@ -66,4 +66,4 @@ $(function () {
   });
 });
 
-console.log('Hello');
+// Smooth scroll
