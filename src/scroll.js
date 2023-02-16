0634a5a4fdad2aa5df56fc64e25d72cb7cf8f8d4
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-02-16 15:21:17 +0200

    check errors

--- src/scroll.js
+++ src/scroll.js
@@ -26,15 +26,6 @@
 //   navLinks.forEach(link => {
 //     link.addEventListener('click', function (e) {
 //       e.preventDefault();
-
-//       // Remove active class from other links
-//       navLinks.forEach(otherLink => {
-//         otherLink.classList.remove('active');
-//       });
-
-//       // Add active class to clicked link
-//       this.classList.add('active');
-
 //       const target = document.querySelector(this.getAttribute('href'));
 //       if (target) {
 //         const offsetTop = target.offsetTop - 100;
@@ -42,6 +33,9 @@
 //           top: offsetTop,
 //           behavior: 'smooth',
 //         });
+
+//         navLinks.forEach(link => link.classList.remove('active'));
+//         this.classList.add('active');
 //       }
 //     });
 //   });
@@ -64,27 +58,45 @@
 
 //   // Update active navigation link
 //   function setActiveLink() {
-//     const currentPos = window.scrollY + headerHeight + 10;
+//     const offset = 100;
+//     const currentPos = window.scrollY + headerHeight + offset;
 //     document.querySelectorAll('section').forEach(section => {
+//       const id = section.getAttribute('id');
+//       const link = document.querySelector(`#nav a[href="#${id}"]`);
 //       if (
-//         section.offsetTop <= currentPos &&
-//         section.offsetTop + section.offsetHeight > currentPos
+//         section.offsetTop - headerHeight - offset <= currentPos &&
+//         section.offsetTop + section.offsetHeight - headerHeight > currentPos
 //       ) {
-//         const id = section.getAttribute('id');
-//         navLinks.forEach(link => {
-//           link.classList.remove('active');
-//           if (link.getAttribute('href') === `#${id}`) {
-//             link.classList.add('active');
-//           }
-//         });
+//         link.classList.add('active');
+//       } else {
+//         link.classList.remove('active');
 //       }
 //     });
 //   }
 
 //   window.addEventListener('scroll', setActiveLink);
-// });
 
-//
+//   const navtoggle = document.querySelector('#nav_toggle');
+//   const nav = document.querySelector('#nav');
+
+//   navtoggle.addEventListener('click', function (event) {
+//     event.preventDefault();
+
+//     navToggle.classList.toggle('active');
+//     nav.classList.toggle('active');
+//   });
+
+//   // Collapse
+//   const collapseElements = document.querySelectorAll('[data-collapse]');
+
+//   collapseElements.forEach(function (element) {
+//     element.addEventListener('click', function (event) {
+//       event.preventDefault();
+
+//       element.classList.toggle('active');
+//     });
+//   });
+// });
 
 document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('#header');
@@ -152,15 +164,41 @@ document.addEventListener('DOMContentLoaded', () => {
       const id = section.getAttribute('id');
       const link = document.querySelector(`#nav a[href="#${id}"]`);
       if (
+        link &&
         section.offsetTop - headerHeight - offset <= currentPos &&
         section.offsetTop + section.offsetHeight - headerHeight > currentPos
       ) {
         link.classList.add('active');
-      } else {
+      } else if (link) {
         link.classList.remove('active');
       }
     });
   }
 
   window.addEventListener('scroll', setActiveLink);
+
+  const navtoggle = document.querySelector('#nav_toggle');
+  const nav = document.querySelector('#nav');
+
+  if (navtoggle && nav) {
+    navtoggle.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      navToggle.classList.toggle('active');
+      nav.classList.toggle('active');
+    });
+  }
+
+  // Collapse
+  const collapseElements = document.querySelectorAll('[data-collapse]');
+
+  collapseElements.forEach(function (element) {
+    if (element) {
+      element.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        element.classList.toggle('active');
+      });
+    }
+  });
 });
