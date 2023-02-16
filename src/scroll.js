b447da20379b7688c6e3ae2bc887951f090698b1
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-02-16 14:41:20 +0200

    smooth scroll

--- src/scroll.js
+++ src/scroll.js
@@ -21,65 +21,84 @@ document.addEventListener('DOMContentLoaded', () => {
       header.classList.remove('fixed');
     }
   }
+});
 
-  // Smooth scrolling and active link
-  navLinks.forEach(link => {
-    link.addEventListener('click', function (e) {
-      e.preventDefault();
-
-      // Remove active class from other links
-      navLinks.forEach(otherLink => {
-        otherLink.classList.remove('active');
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      const offsetTop = target.offsetTop - 100; // Subtract a value to set the link to active just below the specified section boundary
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth',
       });
 
-      // Add active class to clicked link
-      this.classList.add('active');
-
-      const target = document.querySelector(this.getAttribute('href'));
-      if (target) {
-        const offsetTop = target.offsetTop - 100;
-        window.scrollTo({
-          top: offsetTop,
-          behavior: 'smooth',
-        });
-      }
-    });
-  });
-
-  // Fixed header
-  const headerHeight = header.offsetHeight;
-  const headerTop = header.getBoundingClientRect().top + window.pageYOffset;
-
-  function fixHeader() {
-    if (window.pageYOffset > headerTop) {
-      document.body.style.paddingTop = headerHeight + 'px';
-      header.classList.add('fixed');
-    } else {
-      document.body.style.paddingTop = 0;
-      header.classList.remove('fixed');
+      // Set link to active just below the specified section boundary
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+        const section = document.querySelector(link.hash);
+        if (
+          section.offsetTop <= offsetTop &&
+          section.offsetTop + section.offsetHeight > offsetTop
+        ) {
+          link.classList.add('active');
+        }
+      });
     }
-  }
-
-  window.addEventListener('scroll', fixHeader);
-
-  // Update active navigation link
-  function setActiveLink() {
-    const currentPos = window.scrollY + headerHeight + 10;
-    document.querySelectorAll('section').forEach(section => {
-      if (
-        section.offsetTop <= currentPos &&
-        section.offsetTop + section.offsetHeight > currentPos
-      ) {
-        const id = section.getAttribute('id');
-        navLinks.forEach(link => {
-          link.classList.remove('active');
-          if (link.getAttribute('href') === `#${id}`) {
-            link.classList.add('active');
-          }
-        });
-      }
-    });
-  }
-
-  window.addEventListener('scroll', setActiveLink);
+  });
 });
+
+// Fixed header
+const header = document.querySelector('.header');
+const headerHeight = header.offsetHeight;
+const headerTop = header.getBoundingClientRect().top + window.pageYOffset;
+
+function fixHeader() {
+  if (window.pageYOffset > headerTop) {
+    document.body.style.paddingTop = headerHeight + 'px';
+    header.classList.add('fixed');
+  } else {
+    document.body.style.paddingTop = 0;
+    header.classList.remove('fixed');
+  }
+}
+
+window.addEventListener('scroll', fixHeader);
+
+// Update active navigation link
+function setActiveLink() {
+  const offset = 100; // adjust this value to set the offset from the section boundary
+  const currentPos = window.scrollY + headerHeight + offset;
+  document.querySelectorAll('section').forEach(section => {
+    const id = section.getAttribute('id');
+    const link = document.querySelector(`#nav a[href="#${id}"]`);
+    if (
+      section.offsetTop - headerHeight - offset <= currentPos &&
+      section.offsetTop + section.offsetHeight - headerHeight > currentPos
+    ) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+}
+
+// const navLinks = document.querySelectorAll('#nav a[href^="#"]');
+
+// window.addEventListener('scroll', () => {
+//   const scrollDistance = window.scrollY;
+//   navLinks.forEach(link => {
+//     const section = document.querySelector(link.hash);
+//     if (
+//       section.offsetTop <= scrollDistance + headerHeight + 10 &&
+//       section.offsetTop + section.offsetHeight >
+//         scrollDistance + headerHeight + 10
+//     ) {
+//       link.classList.add('active');
+//     } else {
+//       link.classList.remove('active');
+//     }
+//   });
+// });
