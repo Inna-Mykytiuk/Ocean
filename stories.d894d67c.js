7a0d149a8bfd1d86fdcdbba7814518c84f71a283
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-02-19 08:32:59 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/Ocean@0712a8c00b9be31cb0b57b8d0c13ce830f2ddfd8 🚀

---
+++ stories.d894d67c.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelectorAll(".cards_item");e.forEach((s=>{s.addEventListener("mouseover",(()=>{e.forEach((e=>{e!==s&&e.classList.add("blur")})),s.classList.add("hover")})),s.addEventListener("mouseout",(()=>{e.forEach((e=>{e.classList.remove("blur"),e.classList.remove("hover")}))}))}))}();
+//# sourceMappingURL=stories.d894d67c.js.map
