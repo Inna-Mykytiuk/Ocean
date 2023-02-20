94dfbc4ef3f9c1c25b7271266d7bc3380b9b93e9
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-02-20 20:17:58 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/Ocean@f759c9da7e49df3eccd76adb9c551db1b423ab02 🚀

---
+++ index.ff93a080.js
@@ -0,0 +1,2 @@
+const t=document.querySelectorAll("img, video");/iPad|iPhone|iPod/.test(navigator.userAgent)&&!window.MSStream&&t.forEach((t=>{t.setAttribute("playsinline",""),t.setAttribute("controls","")}));
+//# sourceMappingURL=index.ff93a080.js.map
