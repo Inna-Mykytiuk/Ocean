c7c3ecf3cb9e170a071783a709a2873c67129256
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-02-20 22:45:54 +0200

    update

--- src/img.js
+++ src/img.js
@@ -1,8 +1,17 @@
-const device = navigator.userAgent.toLowerCase();
-const mob =
-  /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(device);
+// const device = navigator.userAgent.toLowerCase();
+// const mob =
+//   // /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(device);
+//   /webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(device);
 
-if (mob) {
+// if (mob) {
+//   const bgCover = document.querySelector('.bg-cover');
+//   bgCover.classList.remove('bg-fixed');
+// }
+
+const device = navigator.userAgent.toLowerCase();
+const isIOS = /(iPhone|iPad|iPod)/.test(device);
+
+if (isIOS) {
   const bgCover = document.querySelector('.bg-cover');
   bgCover.classList.remove('bg-fixed');
 }
