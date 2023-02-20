be8700a1d73479406ce7598aa020fbebd5a0c4b3
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-02-21 00:16:33 +0200

    update

--- src/img.js
+++ src/img.js
@@ -8,23 +8,31 @@
 //   bgCover.classList.remove('bg-fixed');
 // }
 
-// const device = navigator.userAgent.toLowerCase();
-// const isIOS = /(iPhone|iPad|iPod)/.test(device);
+// main-header bg-cover bg-fixed
 
-// if (isIOS) {
-//   const bgCover = document.querySelector('.bg-cover');
-//   bgCover.classList.remove('bg-fixed');
+// .main-header {
+//   background-image: url(../images/octopus@2.webp);
+//   width: 100%;
+//   height: 450px;
 // }
-
+// .bg-cover {
+//   background-repeat: no-repeat;
+//   background-position: center center;
+//   background-size: cover;
+//   -webkit-background-size: cover;
+// }
+// .bg-fixed {
+//   background-attachment: fixed;
+// }
 const device = navigator.userAgent.toLowerCase();
 const mob = /webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(
   device
 );
 
 if (mob) {
-  const bgCover = document.querySelector('.bg-cover');
-  bgCover.classList.remove('bg-fixed');
+  // const bgCover = document.querySelector('.bg-cover');
+  // bgCover.classList.remove('bg-fixed');
 
-  const hiddenPart = document.querySelector('.hero3');
+  const hiddenPart = document.querySelector('.hero3, .hero2');
   hiddenPart.style.display = 'none';
 }
