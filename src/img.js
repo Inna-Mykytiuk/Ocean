e362ccaaeed921713e0d6551bc2dc4509fa4eb44
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-02-20 22:59:32 +0200

    update

--- src/img.js
+++ src/img.js
@@ -1,17 +1,17 @@
-// const device = navigator.userAgent.toLowerCase();
-// const mob =
-//   // /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(device);
-//   /webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(device);
-
-// if (mob) {
-//   const bgCover = document.querySelector('.bg-cover');
-//   bgCover.classList.remove('bg-fixed');
-// }
-
 const device = navigator.userAgent.toLowerCase();
-const isIOS = /(iPhone|iPad|iPod)/.test(device);
+const mob =
+  // /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(device);
+  /webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(device);
 
-if (isIOS) {
+if (mob) {
   const bgCover = document.querySelector('.bg-cover');
   bgCover.classList.remove('bg-fixed');
 }
+
+// const device = navigator.userAgent.toLowerCase();
+// const isIOS = /(iPhone|iPad|iPod)/.test(device);
+
+// if (isIOS) {
+//   const bgCover = document.querySelector('.bg-cover');
+//   bgCover.classList.remove('bg-fixed');
+// }
