34ab1d99d349d62f79c90f94b26e3953b0ac0ded
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-02-27 21:54:35 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/Ocean@8ac220f321cb96abfcc4c3ac041fa8bcd71052b9 🚀

--- stories.227f7383.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll(".music-box__btn").forEach((e=>{e.addEventListener("click",(()=>{const c=e.querySelector("audio");c.paused?c.play():(c.pause(),c.currentTime=0)}))}));
-//# sourceMappingURL=stories.227f7383.js.map
---
+++ stories.d5f49ef9.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".music-box__btn").forEach((e=>{const c=e.querySelector(".music__icon");e.addEventListener("click",(()=>{const s=e.querySelector("audio");s.paused?(s.play(),e.classList.add("active"),c.classList.add("active")):(s.pause(),s.currentTime=0,e.classList.remove("active"),c.classList.remove("active"))}))}));
+//# sourceMappingURL=stories.d5f49ef9.js.map
