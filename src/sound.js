0e93ad8a4d46defe28f09feb3ce023d6902a86ce
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-02-28 02:11:26 +0200

    Revert "add active class"
    This reverts commit 8ac220f321cb96abfcc4c3ac041fa8bcd71052b9.

--- src/sound.js
+++ src/sound.js
@@ -1,41 +1,20 @@
-// // Get all the buttons in the music box
-// const buttons = document.querySelectorAll('.music-box__btn');
-
-// // Add a click event listener to each button
-// buttons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     // Get the audio element inside the button
-//     const audio = button.querySelector('audio');
-
-//     if (audio.paused) {
-//       // If the audio is paused, play it
-//       audio.play();
-//     } else {
-//       // If the audio is playing, pause it
-//       audio.pause();
-//       // Reset the playback time to the beginning
-//       audio.currentTime = 0;
-//     }
-//   });
-// });
-
+// Get all the buttons in the music box
 const buttons = document.querySelectorAll('.music-box__btn');
 
+// Add a click event listener to each button
 buttons.forEach(button => {
-  const icon = button.querySelector('.music__icon');
-
   button.addEventListener('click', () => {
+    // Get the audio element inside the button
     const audio = button.querySelector('audio');
 
     if (audio.paused) {
+      // If the audio is paused, play it
       audio.play();
-      button.classList.add('active');
-      icon.classList.add('active');
     } else {
+      // If the audio is playing, pause it
       audio.pause();
+      // Reset the playback time to the beginning
       audio.currentTime = 0;
-      button.classList.remove('active');
-      icon.classList.remove('active');
     }
   });
 });
